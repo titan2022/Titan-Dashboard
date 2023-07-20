85ea658dba8dd58bf3763cc14623ee354abcec2b
nekiwo <nekiwo@users.noreply.github.com>
2023-07-19 22:18:26 -0500

    parse vector

--- src/networking/UDPServer.js
+++ src/networking/UDPServer.js
@@ -30,6 +30,22 @@ module.exports =  class UDPServer extends EventEmitter {
     }
 
     handleData(msg) {
+        const type = String.fromCharCode(msg[0]);
         
+        switch (type) {
+            case 'd':
+                this.handleVector(msg);
+                break;
+        }
+    }
+
+    handleVector(msg) {
+        const x = msg.readDoubleLE(8);
+        const y = msg.readDoubleLE(16);
+        const z = msg.readDoubleLE(24);
+        let name = msg.slice(40, msg.length);
+        name = name.toString().slice(0, name.indexOf("\n")).replace(/\W/g, "");
+        
+        this.emit(name, [x, y, z]);
     }
 }
