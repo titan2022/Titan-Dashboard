33cf13501f15f2d6804917ac4d1808f6c9dcf9f2
nekiwo <nekiwo@users.noreply.github.com>
2023-07-20 13:26:05 -0500

    gutters

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,5 +1,5 @@
 import { Scene } from "./graphics/Scene.js"
-import * as THREE from "three";
+//import Split from window.Split
 var ipcRenderer = require("electron").ipcRenderer;
 
 let scene = new Scene();
@@ -10,3 +10,29 @@ ipcRenderer.on("pos", (event, pos) => {
     scene.robot.position.y = pos[1];
     scene.robot.position.z = -pos[2];
 });
+
+window.onload = () => {
+    window.Split({
+        columnGutters: [{
+            track: 1,
+            element: document.querySelector(".c2"),
+        },
+        {
+            track: 3,
+            element: document.querySelector(".c4"),
+        }],
+        rowGutters: [{
+            track: 1,
+            element: document.querySelector(".mr2"),
+        },
+        {
+            track: 1,
+            element: document.querySelector(".lr2"),
+        },
+        {
+            track: 1,
+            element: document.querySelector(".rr2"),
+        }],
+        onDrag: () => {scene.resizeListener()}
+    });
+}
