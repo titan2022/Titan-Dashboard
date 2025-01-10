f8c28384faa888e998317e205cf34f66f46763d5
ethanc8 <echaroenpitaks@gmail.com>
2025-01-09 19:43:29 -0600

    Fix tag positions (causes quaternion issues)

--- src/ui/graphics/Scene.js
+++ src/ui/graphics/Scene.js
@@ -69,11 +69,21 @@ export class Scene {
             tagMesh.quaternion.set(tag.pose.rotation.quaternion.W, tag.pose.rotation.quaternion.Y, tag.pose.rotation.quaternion.Z, tag.pose.rotation.quaternion.X);
             tagMesh.quaternion.normalize();
         
-            const rotationQuat = new THREE.Quaternion();
-            rotationQuat.setFromAxisAngle(new THREE.Vector3(0, 1, 0), ninety); 
+            let rotationQuat = null;
 
+            // 90-degree Y-axis rotation
+            rotationQuat = new THREE.Quaternion();
+            rotationQuat.setFromAxisAngle(new THREE.Vector3(0, 1, 0), ninety); 
             tagMesh.quaternion.multiply(rotationQuat);
-            tagMesh.position.set(tag.pose.translation.x-apriltags.field.length/2, tag.pose.translation.z, tag.pose.translation.y-apriltags.field.width/2);
+
+            // Flip around the Z-axis to make it 
+            rotationQuat = new THREE.Quaternion();
+            rotationQuat.setFromAxisAngle(new THREE.Vector3(0, 0, 1), 2*ninety); 
+            tagMesh.quaternion.multiply(rotationQuat);
+            
+            // tagMesh.position.set(tag.pose.translation.x-apriltags.field.length/2, tag.pose.translation.z, tag.pose.translation.y-apriltags.field.width/2);
+            tagMesh.position.set(tag.pose.translation.x-apriltags.field.length/2, tag.pose.translation.z, -(tag.pose.translation.y-apriltags.field.width/2));
+
             this.tags.push(tagMesh);
             this.scene.add(this.tags.at(-1));
 
